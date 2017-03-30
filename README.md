9ee83cfde673754438592b32993b15411c461e74
Francesco Visin <fvisin@gmail.com>
2017-03-30 19:21:53 +0200

    First working version

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# py_bulk_email
-A python script to send emails to multiple addresses from an xls or xlsx list
+A python script to send emails to multiple addresses from an 'xls' or '.xlsx'
+list.
+
+THE SOFTWARE IS PROVIDED AS IS AND **NO SUPPORT** WILL BE GIVEN.
+
+## Installation
+This script depends on `pyexcel_xls`. 
+On Mac or Linux, from a terminal execute (you might need to install pip 
+before):
+
+```bash
+pip install --user pyexcel_xls
+```
+
+## Use
+See the html in py_bulk_email.xlsx for an example of the usage of this tool.
+The script allows you to add inline images just by:
+    * Copying the images into the inline_images directory (take note of their
+      alphabetical order!) 
+    * Writing the usual html code for images <img src="cid:image#"> replacing
+      the # with an increasing image number that follows the alphabetical order
+      of the images.
+
+You can easily put placeholders in the html with curly braces, that will be
+replaced by the corresponding field in the Contacts sheet.
+
+Finally, you can specify some information that will be used to build the email
+in the Email content sheet:
+    * **subject:** the subject of the emails 
+    * **primary email field:** the email that will be used as first option
+    * **secondary email field":** the email that will be used if the primary
+      email field is empty
+
+Of course you can also add attachments! To do so you just need to put them in
+the attachments directory.
---
+++ py_bulk_email.py
@@ -0,0 +1,108 @@
+from email.mime.application import MIMEApplication
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formatdate
+import os
+import smtplib
+
+from pyexcel_xls import get_data
+
+
+def rows_to_dicts(lst):
+    headers = lst[0]
+    out_lst = []
+    for el in lst[1:]:
+        out_lst.append({k: v for k, v in zip(headers, el)})
+    return out_lst
+
+
+def cols_to_dicts(lst):
+    headers = [el[0] for el in lst]
+    out_lst = []
+    for el_idx in range(1, len(lst[0])):
+        out_lst.append({k: v for k, v in zip(headers,
+                                             [el[el_idx] for el in lst])})
+    return out_lst
+
+
+def batch_send_email(xls='py_bulk_email.xlsx'):
+    # Get data from xls
+    try:
+        try:
+            data = get_data(xls)
+        except IOError:
+            data = get_data(xls[:-1])
+    except IOError:
+        raise IOError('No such files: send_email.xlsx nor send_email.xls. '
+                      'You need to provide an xls file with the email '
+                      'information')
+
+    # Get the data out
+    account_info = rows_to_dicts(data['Account info'])[0]
+    email_content = cols_to_dicts(data['Email content'])[0]
+    contacts = rows_to_dicts(data['Contacts'])
+
+    subject = email_content['subject'].encode('utf-8').strip()
+    prim_email_f = email_content['primary email field'].encode('utf-8').strip()
+    sec_email_f = email_content['secondary email field'].encode(
+        'utf-8').strip()
+
+    html = email_content['html'].encode('utf-8')
+
+    from_email = account_info['email'].encode('utf-8').strip()
+    smtp = account_info['smtp'].encode('utf-8').strip()
+    port = account_info['port']
+    username = account_info['username'].encode('utf-8').strip()
+    password = account_info['password'].encode('utf-8').strip()
+
+    # Connect and authenticate
+    try:
+        mail = smtplib.SMTP(smtp, port, timeout=5)
+        mail.ehlo_or_helo_if_needed()
+        mail.starttls()
+    except smtplib.SMTPServerDisconnected:
+        mail = smtplib.SMTP_SSL(smtp, port, timeout=5)
+        mail.ehlo_or_helo_if_needed()
+    mail.login(username, password)
+
+    for ct in contacts:
+        # Create message container
+        msg = MIMEMultipart('related')
+        msg['Subject'] = subject
+        msg['From'] = from_email
+        msg['Date'] = formatdate(localtime=True)
+        msg.preamble = 'This is a multi-part message in MIME format.'
+
+        # Create the body of the message
+        # Record the MIME type - text/html
+        text = MIMEText(html.format(**ct), 'html')
+        msg.attach(text)
+
+        # Load attachments
+        for fname in os.listdir('attachments'):
+            with open(os.path.join('attachments', fname), 'rb') as f:
+                part = MIMEApplication(f.read(), Name=fname)
+                part['Content-Disposition'] = ('attachment; filename="%s"'
+                                               '' % fname)
+                msg.attach(part)
+
+        # Load images
+        for i, fname in enumerate(os.listdir('inline_images')):
+            with open(os.path.join('inline_images', fname), 'rb') as f:
+                img = MIMEImage(f.read(), Name=fname)
+                img.add_header('Content-ID', '<image{}>'.format(i+1))
+                msg.attach(img)
+
+        # Batch send emails
+        to_email = ct[prim_email_f].encode('utf-8')
+        if to_email is None or to_email == '':
+            to_email = ct[sec_email_f].encode('utf-8')
+        msg['To'] = to_email
+        mail.sendmail(from_email, to_email, msg.as_string())
+
+    mail.quit()
+
+
+if __name__ == '__main__':
+    batch_send_email()
